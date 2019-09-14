c7968f6766325293c4a0c1cb250217b82632bc94
Anna Malmgren <am223rj@student.lnu.se>
2019-09-14 12:31:14 +0200

    Adding form validation for register

--- controller/RegisterController.php
+++ controller/RegisterController.php
@@ -3,21 +3,30 @@
 class RegisterController {
     private $username = null;
     private $password = null;
+    private $passwordRepeat = null;
 
     public function __construct() {
         if (isset($_POST["DoRegistration"])) {
-            $this->username = $_POST['RegisterView::UserName'];
-            $this->password = $_POST['RegisterView::Password'];
+            $this->username = trim($_POST['RegisterView::UserName']);
+            $this->password = trim($_POST['RegisterView::Password']);
+            $this->passwordRepeat = trim($_POST['RegisterView::PasswordRepeat']);
         }
     }
 
     public function validateFormInput() {
-        if (empty(trim($this->username)) && $this->username !== null) {
-            return "Username is Missing";
-        } else if (empty(trim($this->password)) && $this->password !==null) {
-            return "Password is Missing";
-        } else {
-            return "";
+        if ($this->username !== null || $this->password !== null || $this->passwordRepeat !== null) {
+            if (empty($this->username) && empty($this->password)) {
+                return "Username has too few characters, at least 3 characters. Password has too few characters, at least 6 characters.";
+            } else if (strlen($this->username) < 3) {
+                return "Username has too few characters, at least 3 characters.";
+            } else if (strlen($this->password) < 6) {
+                return "Password has too few characters, at least 6 characters.";
+            } else if ($this->password !== $this->passwordRepeat) {
+                return "Passwords do not match.";
+            } else {
+                return "User registrated";
+            }
         }
     }
+
 }
---
+++ dbsettingsTemplate.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Template for database settings.
+ */
+$servername = "server";
+$dbUsername = "db username";
+$dbPassword = "db password";
+$dbName = "db name";
