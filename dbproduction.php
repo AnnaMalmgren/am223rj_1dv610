79279f8605cf8684357bc5459d26ab01f1a56563
Anna Malmgren <am223rj@student.lnu.se>
2019-09-29 16:15:18 +0200

    Fixing dbproduction

--- dbproduction.php
+++ dbproduction.php
@@ -5,3 +5,10 @@ $dbUsername = getenv("DB_USERNAME");
 $dbPassword = getenv("DB_PASSWORD");
 $dbName = getenv("DB_DATABASE");
 
+$conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+
+if(!$conn) {
+die('Could not connect to db: ' . mysqli_error());
+}
+
+
